237ffaaeaa944b9278071a9a9bd1f61f133065bd
Juan Diego de Jesus Mas Uicab <jmas@palace-resorts.local>
2018-12-03 13:35:06 -0500

    desabled.js, problemas en version esp el slide

--- sites/all/themes/mice/js/disabled.js
+++ sites/all/themes/mice/js/disabled.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
 if( $('.banner-mobile').length )  {
  document.getElementsByClassName('banner-mobile')[0].childNodes[1].childNodes[3].setAttribute('target','blank');
-  document.getElementsByClassName('slides')[0].childNodes[3].childNodes[3].removeChild(document.getElementsByClassName('slides')[0].childNodes[3].childNodes[3].childNodes[1]);
+  //document.getElementsByClassName('slides')[0].childNodes[3].childNodes[3].removeChild(document.getElementsByClassName('slides')[0].childNodes[3].childNodes[3].childNodes[1]);
 
 }
 
